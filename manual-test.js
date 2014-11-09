fc614e131c300a82bb6136c0a49676a56192a9ec
Sandeep Mistry <sandeep.mistry@gmail.com>
2014-11-09 12:18:16 -0500

    - white space

--- manual-test.js
+++ manual-test.js
@@ -3,57 +3,57 @@ var PowerMate = require('./powermate');
 var powermate = new PowerMate();
 
 powermate.on('buttonUp', function() {
-	console.log('buttonUp');
+  console.log('buttonUp');
 });
 
 powermate.on('buttonDown', function() {
-	console.log('buttonDown');
+  console.log('buttonDown');
 });
 
 powermate.on('wheelTurn', function(delta) {
-	console.log('wheelTurn ' + delta);
+  console.log('wheelTurn ' + delta);
 });
 
 setTimeout(function() {
-	var desiredBrightness = 255;
-	powermate.setBrightness(desiredBrightness, function() {
-		powermate.brightness(function(brightness) {
-			console.log('desiredBrightness = ' + desiredBrightness + ', brightness = ' + brightness);
-		});
-	});
+  var desiredBrightness = 255;
+  powermate.setBrightness(desiredBrightness, function() {
+    powermate.brightness(function(brightness) {
+      console.log('desiredBrightness = ' + desiredBrightness + ', brightness = ' + brightness);
+    });
+  });
 }, 0);
 
 setTimeout(function() {
-	powermate.pulseAwake(function(pulseAwake) {
-		console.log('pulseAwake = ' + pulseAwake);
+  powermate.pulseAwake(function(pulseAwake) {
+    console.log('pulseAwake = ' + pulseAwake);
 
-		var desiredPulseAwake = !pulseAwake;
-		powermate.setPulseAwake(desiredPulseAwake, function() {
-			powermate.pulseAwake(function(pulseAwake) {
-				console.log('desiredPulseAwake = ' + desiredPulseAwake + ', pulseAwake = ' + pulseAwake);
-			});
-		});
-	});
+    var desiredPulseAwake = !pulseAwake;
+    powermate.setPulseAwake(desiredPulseAwake, function() {
+      powermate.pulseAwake(function(pulseAwake) {
+        console.log('desiredPulseAwake = ' + desiredPulseAwake + ', pulseAwake = ' + pulseAwake);
+      });
+    });
+  });
 }, 5000);
 
 setTimeout(function() {
-	powermate.pulseAsleep(function(pulseAsleep) {
-		console.log('pulseAsleep = ' + pulseAsleep);
+  powermate.pulseAsleep(function(pulseAsleep) {
+    console.log('pulseAsleep = ' + pulseAsleep);
 
-		var desiredPulseAsleep = !pulseAsleep;
-		powermate.setPulseAsleep(desiredPulseAsleep, function() {
-			powermate.pulseAsleep(function(pulseAsleep) {
-				console.log('desiredPulseAsleep = ' + desiredPulseAsleep + ', pulseAsleep = ' + pulseAsleep);
-			});
-		});
-	});
+    var desiredPulseAsleep = !pulseAsleep;
+    powermate.setPulseAsleep(desiredPulseAsleep, function() {
+      powermate.pulseAsleep(function(pulseAsleep) {
+        console.log('desiredPulseAsleep = ' + desiredPulseAsleep + ', pulseAsleep = ' + pulseAsleep);
+      });
+    });
+  });
 }, 10000);
 
 setTimeout(function() {
-	var desiredPulseSpeed = 0;
-	powermate.setPulseSpeed(desiredPulseSpeed, function() {
-		powermate.pulseSpeed(function(pulseSpeed) {
-			console.log('desiredPulseSpeed = ' + desiredPulseSpeed + ', pulseSpeed = ' + pulseSpeed);
-		});
-	});
+  var desiredPulseSpeed = 0;
+  powermate.setPulseSpeed(desiredPulseSpeed, function() {
+    powermate.pulseSpeed(function(pulseSpeed) {
+      console.log('desiredPulseSpeed = ' + desiredPulseSpeed + ', pulseSpeed = ' + pulseSpeed);
+    });
+  });
 }, 15000);
